1a374fda225639fa3acf599e41a7a0437224585f
dandom <zxkt2739@sina.com>
2019-08-12 15:59:52 +0800

    Q001 暴力算法

--- src/test/Q001_TwoSum.java
+++ src/test/Q001_TwoSum.java
@@ -11,11 +11,25 @@ Given nums = [2, 7, 11, 15], target = 9,
 Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
  */
+
 public class Q001_TwoSum {
-    public int[] twoSum(int[] nums, int target) {
+    // 暴力算法
+    public int[] twoSum_1(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+
+    public int[] twoSum_2(int[] nums, int target) {
 
 
 
-        return new int[3];
+        return new int[2];
     }
 }
