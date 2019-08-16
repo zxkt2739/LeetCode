e8aa0aba7d50d406b4571665fe191c8d27cd49ea
dandom <zxkt2739@sina.com>
2019-08-16 16:07:59 +0800

    Q0003 修改

---
+++ src/leetcode/Q0003_LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,43 @@
+package leetcode;
+
+/*
+Given a string, find the length of the longest substring without repeating characters.
+
+Example 1:
+
+    Input: "abcabcbb"
+    Output: 3
+    Explanation: The answer is "abc", with the length of 3.
+
+Example 2:
+
+    Input: "bbbbb"
+    Output: 1
+    Explanation: The answer is "b", with the length of 1.
+
+Example 3:
+
+    Input: "pwwkew"
+    Output: 3
+    Explanation: The answer is "wke", with the length of 3.
+        Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
+
+date:
+ */
+
+@SuppressWarnings("unused")
+public class Q0003_LongestSubstringWithoutRepeatingCharacters {
+
+    /*
+    算法分析
+
+     */
+
+    public int lengthOfLongestSubstring(String s) {
+
+
+
+        return 0;
+    }
+}
